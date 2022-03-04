7ee8ef63ae458e3526e5871ceb843f4cf24274f8
M.Inzamam Malik <malikasinger1@gmail.com>
2022-03-04 20:09:33 +0500

    class code for webhook lib

--- 05.dialogflow/4. React integration webhook/server.mjs
+++ 05.dialogflow/4. React integration webhook/server.mjs
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import dialogflow from '@google-cloud/dialogflow';
 import gcHelper from "google-credentials-helper"
+import { WebhookClient, Card, Suggestion } from 'dialogflow-fulfillment';
+
 
 gcHelper();
 const sessionClient = new dialogflow.SessionsClient();
@@ -53,28 +55,84 @@ app.post("/talktochatbot", async (req, res) => {
 
 app.post("/webhook", (req, res) => {
 
-    const params = req.body.queryResult.parameters;
+    const agent = new WebhookClient({ request: req, response: res });
 
-    console.log("params.cityName: ", params.cityName)
+    function welcome(agent) {
+        // agent.add(new Card({
+        //     title: 'Vibrating molecules',
+        //     imageUrl: "https://media.nationalgeographic.org/assets/photos/000/263/26383.jpg",
+        //     text: 'Did you know that temperature is really just a measure of how fast molecules are vibrating around?! 😱',
+        //     buttonText: 'Temperature Wikipedia Page',
+        //     buttonUrl: "https://sysborg.com"
+        // })
+        // );
+        agent.add('Welcome to the Weather Assistant!');
+        agent.add('you can ask me name, or weather updates');
+        agent.add(new Suggestion('what is your name'));
+        agent.add(new Suggestion('Weather update'));
+        agent.add(new Suggestion('Cancel'));
+    }
 
-    // TODO: make api call to weather server
+    function weather(agent) {
+        // Get parameters from Dialogflow to convert
+        const cityName = agent.parameters.cityName;
 
-    res.send({
-        "fulfillmentText": `response from webhok. weather of ${params.cityName} is 17°C.
-                            thank you for calling weather app. good bye.`,
+        console.log(`User requested to city ${cityName}`);
 
-        "fulfillmentMessages": [
-            {
-                "text": {
-                    "text": [
-                        `response from webhoook weather of ${params.cityName} is 17°C.
-                        thank you for calling weather app. good bye.`
-                    ]
-                }
-            }
-        ]
-    })
+        //TODO: Get weather from api
+
+        // Compile and send response
+        agent.add(`in ${cityName} its 27 degree centigrade, would you like to know anything else?`);
+        agent.add(new Suggestion('What is your name'));
+        agent.add(new Suggestion('Hi'));
+        agent.add(new Suggestion('Cancel'));
+    }
+
+    function fallback(agent) {
+        agent.add('Woah! Its getting a little hot in here.');
+        agent.add(`I didn't get that, can you try again?`);
+    }
+
+    let intentMap = new Map(); // Map functions to Dialogflow intent names
+    intentMap.set('Default Welcome Intent', welcome);
+    intentMap.set('weather', weather);
+    intentMap.set('Default Fallback Intent', fallback);
+    agent.handleRequest(intentMap);
+
 })
+
+
+
+
+
+
+
+
+
+// app.post("/webhook", (req, res) => {
+
+//     const params = req.body.queryResult.parameters;
+
+//     console.log("params.cityName: ", params.cityName)
+
+//     // TODO: make api call to weather server
+
+//     res.send({
+//         "fulfillmentText": `response from webhok. weather of ${params.cityName} is 17°C.
+//                             thank you for calling weather app. good bye.`,
+
+//         "fulfillmentMessages": [
+//             {
+//                 "text": {
+//                     "text": [
+//                         `response from webhoook weather of ${params.cityName} is 17°C.
+//                         thank you for calling weather app. good bye.`
+//                     ]
+//                 }
+//             }
+//         ]
+//     })
+// })
 
 
 app.get("/profile", (req, res) => {
